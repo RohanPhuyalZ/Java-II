53c28be7685c74a1d4aec3811ec6da30f61677cf
RoNzzzz <rohanphuyal2@gmail.com>
2023-05-13 12:00:48 +0545

    L2-Complete

---
+++ src/Lab2/Question12.java
@@ -0,0 +1,38 @@
+package Lab2;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Question12 {
+    Question12(){
+        Frame frame = new Frame();
+        frame.setSize(600,600);
+        frame.setLayout(new FlowLayout());
+        Dialog d1 = new Dialog(frame,"Test",true);
+        FileDialog d2 = new FileDialog(d1,"Open Fie",FileDialog.LOAD);
+        d1.setSize(200,200);
+        d1.setLayout(new FlowLayout());
+        Button b2 = new Button("File");
+        d1.add(b2);
+        Button b1 = new Button("Dialog");
+        frame.add(b1);
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                d1.setVisible(true);
+            }
+        });
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                d2.setVisible(true);
+            }
+        });
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Question12();
+    }
+}
