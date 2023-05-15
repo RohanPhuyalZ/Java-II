0e9ae368654fbc0bfb3aa245d6e1fbd0df860553
RohanPhuyalZ <rohanphuyal2021@gmail.com>
2023-05-15 15:10:34 +0545

    L3-Q7

--- src/Lab3/Question7.java
+++ src/Lab3/Question7.java
@@ -1,4 +1,44 @@
 package Lab3;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Question7 {
+    Question7(){
+        Frame frame = new Frame();
+        frame.setSize(600,200);
+        frame.setLayout(new FlowLayout());
+
+        Label l1 = new Label("Number");
+        Label l2 = new Label("Factorial");
+        TextField t1 = new TextField(10);
+        TextField t2 = new TextField(10);
+        t2.setEnabled(false);
+        Button b1 = new Button("Calculate");
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int number = Integer.parseInt(t1.getText());
+                int result = fact(number);
+                t2.setText(""+result);
+            }
+        });
+        frame.add(l1);
+        frame.add(t1);
+        frame.add(b1);
+        frame.add(l2);
+        frame.add(t2);
+        frame.setVisible(true);
+    }
+    int fact(int number){
+        if(number==0||number==1){
+            return 1;
+        }else{
+            return number * fact(number-1);
+        }
+    }
+    public static void main(String[] args) {
+        new Question7();
+    }
 }
---
+++ src/Lab3/Question8.java
@@ -0,0 +1,11 @@
+package Lab3;
+
+public class Question8 {
+    Question8(){
+
+    }
+
+    public static void main(String[] args) {
+        new Question8();
+    }
+}
